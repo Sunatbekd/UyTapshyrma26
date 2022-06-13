ae7262bd7a0d7c1fb26a3212eafccb80d29bff41
Admin <sunatbekd@gmail.com>
2022-06-13 19:58:47 +0600

    Update

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -1,9 +1,21 @@
 package com.company;
 
 public class Box <T>{
-    T type;
+      private T type;
 
-    public static <T> Box<T>  box(Box box){
-        return new Box<T>();
+    public Box(T type) {
+        this.type=type;
+    }
+
+
+    public static <T> Box<T>  genericbox(T type){
+        return new Box<T>(type);
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "type=" + type +
+                '}';
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Box.box(new Box());
+        System.out.println(Box.genericbox("It is method return generic method"));
     }
 }
